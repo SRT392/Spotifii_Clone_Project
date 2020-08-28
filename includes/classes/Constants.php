c3768749349f38da2c214266c1b55b9e027d23cf
SRT392 <68977071+SRT392@users.noreply.github.com>
2020-08-27 23:03:20 -0700

    Create Constants.php

---
+++ includes/classes/Constants.php
@@ -0,0 +1,17 @@
+<?php 
+class Constants {
+	public static $passwordsDoNotMatch = "Your passwords do not match";
+	public static $passwordsDoNotAlphanumeric = "Your password can only contain numbers and letters";
+	public static $passwordCharacters = "Your password has to be between 8 and 30 characters";
+	public static $emailInvalid = "Email is invalid";
+	public static $emailsDoNotMatch = "Your emails do not match";
+	public static $emailTaken = "This email is already in use";
+	public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+	public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
+	public static $usernameTaken = "This username is already exists";
+
+	public static $loginFailed = "Your username or password was incorrect";
+
+}
+?>
